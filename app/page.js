7440b7b52437dce373614a79d4e75a21568dd0c3
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-08-22 19:12:48 -0500

    Fix import stripe

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import Image from 'next/image';
-import getStripe from '../utils/get-stripe';
+import getStripe from '../utils/getStripe';
 import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import { useEffect } from 'react';
 import { useAuth } from '@clerk/clerk-react';
