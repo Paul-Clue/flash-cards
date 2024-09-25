8e52c9a1fa616117349dc53eebe91352d34571d2
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-09-25 16:57:42 -0500

    Add google

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { ClerkProvider } from "@clerk/nextjs"
+import { ClerkProvider } from '@clerk/nextjs';
+import Head from 'next/head';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +15,34 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={inter.className}>{children}</body>
+        <Head>
+          <title>Fast-Cards</title>
+          <link rel='icon' href='/favicon.ico' />
+          <meta
+            name='description'
+            content='Create quick hits of knowledge on any topic or subject matter.'
+          />
+        </Head>
+        <Script id='google-tag-manager' strategy='afterInteractive'>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MQ4WG4TP');
+          `}
+        </Script>
+        <body className={inter.className}>
+          <Script id='google-tag-manager-noscript' strategy='afterInteractive'>
+            {`
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQ4WG4TP"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+          `}
+          </Script>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ import getStripe from '../utils/getStripe';
 import { SignedIn, SignedOut, useUser } from '@clerk/nextjs';
 import { useEffect, useState } from 'react';
 import { Typography, Container, Grid, Button, Box } from '@mui/material';
-import Head from 'next/head';
+// import Head from 'next/head';
+// import Script from 'next/script';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import BottomNav from './components/BottomNav';
@@ -125,7 +126,7 @@ export default function Home() {
         height: '100%',
       }}
     >
-      <Head>
+      {/* <Head>
         <title>Fast-Cards</title>
         <link rel='icon' href='/favicon.ico' />
         <meta
@@ -133,6 +134,15 @@ export default function Home() {
           content='Create quick hits of knowledge on any topic or subject matter.'
         />
       </Head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MQ4WG4TP');
+          `}
+        </Script> */}
       <Navbar />
       {/* <Box
         sx={{
