a006a76ce40cf175eab3910a626b1161c37d864a
Paul Clue <67766160+Paul-Clue@users.noreply.github.com>
2024-09-27 15:51:17 -0500

    Add pixel

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,23 @@ export default function RootLayout({ children }) {
             content='Create quick hits of knowledge on any topic or subject matter.'
           />
         </Head>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Script id='facebook-pixel' strategy='afterInteractive'>
+            {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '813537817356776');
+            fbq('track', 'PageView');
+          `}
+          </Script>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
